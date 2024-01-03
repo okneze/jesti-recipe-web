6c4262feceb277f99b859e481be76c978c99cd52
petrosh <github@petrosh.de>
2024-01-03 01:00:16 +0100

    Repetition bars in grid, grid alignment

--- src/Sheet.tsx
+++ src/Sheet.tsx
@@ -26,8 +26,35 @@ function Sheet({data}: {data: SheetType}) {
     return reactStringReplace(line, /\[(.*?)\]/g, (value) => (<div className={chordClasses}>{new Chord(value, sheet.key).transpose(sheet.capo).toString()}</div>))
   }
 
-  function parseGrid(line: string|React.ReactNode[]): React.ReactNode[] {
-    return [<div className={styles['chord-grid']}>{reactStringReplace(line, /\|([^|]*)/g, (value) => value !== "" && (<div className={styles['chord-bar']}>{parseChord(value)}</div>))}</div>]
+  function parseGrid(line: string): React.ReactNode[] {
+    // special bars
+    const classes: Record<number, string> = {};
+    const bars = /(?<double>\|\|)|(?<esorep>:\|:)|(?<sorep>\|:)|(?<eorep>:\|)/g;
+    let off = 0;
+    for(const match of line.matchAll(bars)) {
+      const cls = Object.entries(match.groups ?? []).reduce((prev, obj) => obj[1] !== undefined ? obj[0] : prev, "");
+      if(match.index !== undefined) {classes[match.index - off] = styles[`bar-${cls}`]};
+      off += match[0].length - 1;
+    }
+    line = line.replaceAll(bars, "|");
+    function getClasses(id: number) {
+      return Object.keys(classes).includes(`${id}`) ? classes[id] : undefined;
+    }
+    let barCounter = 0;
+    return [<div className={styles['chord-grid']}>{
+      reactStringReplace(line, /\|([^|]*)/g, (value, _, offset) => {
+        barCounter += 1;
+        if (value !== "") {
+          return (
+            <div className={classNames(styles['chord-bar'], getClasses(offset + barCounter - 1))}>
+              {parseChord(value)}
+            </div>
+          )
+        } else if(getClasses(offset + barCounter - 1)) {
+          return (<div className={classNames(getClasses(offset + barCounter - 1))}></div>)
+        }
+      })
+    }</div>]
   }
 
   function directiveDefine(directive: string) {
@@ -90,7 +117,7 @@ function Sheet({data}: {data: SheetType}) {
                 const modifications: (string|React.ReactNode[])[] = [line];
 
                 if(directiveMode === 'grid') {
-                  modifications.unshift(parseGrid(modifications[0]));
+                  modifications.unshift(parseGrid(line));
                 } else {
                   // parse chords
                   modifications.unshift(parseBlock(modifications[0]));
