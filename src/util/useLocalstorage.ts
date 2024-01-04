fddc58cc6798c37f9034836197c062b8c6fc52cd
petrosh <github@petrosh.de>
2024-01-04 01:47:21 +0100

    fix linter

--- src/util/useLocalstorage.ts
+++ src/util/useLocalstorage.ts
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 
-export default function<T>(name: string, value: T): [T, (i: T) => void] {
+function useLocalstorage<T>(name: string, value: T): [T, (i: T) => void] {
     const [item, setItem] = useState(() => {
         return JSON.parse(localStorage.getItem(name) ?? "false") as T || value;
     });
     useEffect(() => {
         localStorage.setItem(name, JSON.stringify(item));
-    }, [item]);
+    }, [item, name]);
     return [item, setItem];
 }
+
+export default useLocalstorage;
